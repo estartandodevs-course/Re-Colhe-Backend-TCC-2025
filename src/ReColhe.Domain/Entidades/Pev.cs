333f15ee2e20f47f816f7a297cb7c844096f7270
Eduardo <eduardohairescampos@gmail.com>
2025-11-08 21:02:41 -0300

    feat/ add entidade e interface PEV(pontos de entrega voluntarios)

---
+++ src/ReColhe.Domain/Entidades/Pev.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReColhe.Domain.Entidades
+{
+    /// <summary>
+    /// Representa a entidade Pev (Ponto de Entrega Voluntária) no banco de dados.
+    /// Mapeia a tabela 'PEV'.
+    /// </summary>
+    public class Pev
+    {
+        [Key] // Chave Primária
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int PevId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Nome { get; set; }
+
+        [MaxLength(500)]
+        public string Endereco { get; set; } 
+
+        [MaxLength(20)]
+        public string Telefone { get; set; } 
+
+        [MaxLength(255)]
+        public string HorarioFuncionamento { get; set; }
+
+        [MaxLength(255)]
+        public string Materiais { get; set; } 
+
+        // Vem do campo "position"
+        [Column(TypeName = "decimal(10, 8)")]
+        public decimal Latitude { get; set; } // position[0]
+
+        [Column(TypeName = "decimal(11, 8)")]
+        public decimal Longitude { get; set; } // position[1]
+
+        // relacao com a tabela intermediaria com o ususario
+        //public virtual ICollection<UsuarioPevFavorito> FavoritadoPor { get; set; }
+
+        //public Pev()
+        //{
+        //    FavoritadoPor = new HashSet<UsuarioPevFavorito>();
+        //}
+    }
+}
---
+++ src/ReColhe.Domain/Repository/IPevRepository.cs
@@ -0,0 +1,30 @@
+﻿using ReColhe.Domain.Entidades; // Importa as entidades
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReColhe.Domain.Repository
+{
+    /// <summary>
+    /// Contrato (Interface) para o repositório de PEVs.
+    /// Define as operações de banco de dados necessárias para a entidade PEV.
+    /// </summary>
+    public interface IPevRepository
+    {
+        // CRUD Básico
+        Task<IEnumerable<Pev>> GetAllAsync();
+        Task<Pev> GetByIdAsync(int pevId);
+        Task AddAsync(Pev pev);
+        Task UpdateAsync(Pev pev);
+        Task DeleteAsync(int pevId);
+
+        // Rotas do
+        //Task<IEnumerable<Pev>> GetFavoritosByUsuarioIdAsync(int usuarioId);
+        //Task AddFavoritoAsync(UsuarioPevFavorito favorito);
+        //Task RemoveFavoritoAsync(UsuarioPevFavorito favorito);
+
+        //// Métodos auxiliares
+        //Task<UsuarioPevFavorito> GetFavoritoAsync(int usuarioId, int pevId);
+        //Task<bool> PevExistsAsync(int pevId);
+        Task<bool> SaveChangesAsync();
+    }
+}
